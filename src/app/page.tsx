c023d8d9939fd3c0b1d4d2ad7d099e9f28b11c48
Amethesh <ametheshlgp@gmail.com>
2023-12-24 23:59:39 +0530

    Style: Add all ui components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,17 @@
+'use client';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/Avatar';
 import { Button } from '@/components/ui/Button';
+
 import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 ">
+    <main className="flex min-h-screen flex-col items-center p-24 ">
       <Button variant={'primary'}>Click Me</Button>
-      <Button variant={'destructive'}>Click Me</Button>
-      <Button variant={'ghost'}>Click Me</Button>
-      <Button variant={'link'}>Click Me</Button>
-      <Button variant={'outline'}>Click Me</Button>
-      <Button variant={'secondary'}>Click Me</Button>
-      <Button variant={'dark'}>Click Me</Button>
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </main>
   );
 }
