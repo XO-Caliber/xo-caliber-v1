20dfa68bc0ac6618f19c7bacdc7e72da91377f41
Amethesh <ametheshlgp@gmail.com>
2023-12-24 23:27:57 +0530

    Style: Add css variable and setup shadcn custom button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,16 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "XO Caliber",
-  description: "Cover letter generator app",
+  title: 'XO Caliber',
+  description: 'Cover letter generator app'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: {
   children: React.ReactNode;
 }) {
