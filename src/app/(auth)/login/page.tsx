2774246dc57a8de062db67d44f6a5ef97c7140ca
Amethesh <ametheshlgp@gmail.com>
2023-12-25 23:29:29 +0530

    Style: Finish login page background

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,10 @@
+import { LoginBox } from '@/components/pages/login/LoginBox';
 import logo from '../../../../public/images/LOGO_Trans.png';
 import Image from 'next/image';
 const page = () => {
   return (
     <section
-      className="absolute top-0 left-0 w-full h-screen overflow-hidden
+      className="absolute top-0 left-0 w-full h-full overflow-hidden
                 bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-rose-200 to-white"
     >
       <Image
@@ -15,10 +16,21 @@ const page = () => {
         height={200}
       />
       <div
-        className="absolute -right-96 w-[620px] h-[629px]
-                      bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a62]
+        className="absolute top-1/2 left-1/2 translate-x-[85%] -translate-y-1/2 w-[620px] h-[620px]
+                      bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
+                      rounded-[135px] rotate-45 shadow-custom-shadow"
+      ></div>
+      <div
+        className="absolute -top-96 right-52 w-[620px] h-[620px]
+                      bg-gradient-to-t from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
                       rounded-[135px] transform rotate-45 shadow-custom-shadow"
       ></div>
+      <div
+        className="absolute -bottom-96 right-52  w-[620px] h-[620px]
+                      bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
+                      rounded-[135px] transform rotate-45 shadow-custom-shadow"
+      ></div>
+      <LoginBox />
     </section>
   );
 };
---
+++ src/components/pages/login/LoginBox.tsx
@@ -0,0 +1,4 @@
+
+export const LoginBox = () => {
+  return <div>LoginBox</div>;
+};
