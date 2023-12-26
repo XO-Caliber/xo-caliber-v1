599372bb72d9a44ff4521488522d769ab5408c3b
Amethesh <ametheshlgp@gmail.com>
2023-12-26 19:26:34 +0530

    style: making login and signup responsive

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,26 +4,36 @@ import Image from "next/image";
 const page = () => {
   return (
     <section
-      className="absolute top-0 left-0 w-full h-full overflow-hidden
+      className="absolute top-0 left-0 
+                w-full h-full overflow-hidden
                 bg-[radial-gradient(ellipse_at_left,_var(--tw-gradient-stops))] from-rose-200 to-white"
     >
       <Image className="absolute m-4" priority src={logo} alt="Logo" width={250} height={200} />
+
+      {/* Hide on mobile, show on larger screens | Top one */}
       <div
-        className="absolute top-1/2 left-1/2 translate-x-[85%] -translate-y-1/2 w-[620px] h-[620px]
-                      bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
-                      rounded-[135px] rotate-45 shadow-custom-shadow"
+        className="absolute -top-96 right-52 
+                  w-[620px] h-[620px]
+                  bg-gradient-to-t from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
+                  rounded-[135px] transform rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
+
+      {/* Hide on mobile, show on larger screens | Right one */}
       <div
-        className="absolute -top-96 right-52 w-[620px] h-[620px]
-                      bg-gradient-to-t from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
-                      rounded-[135px] transform rotate-45 shadow-custom-shadow"
+        className="absolute top-1/2 left-1/2 translate-x-[85%] -translate-y-1/2 
+        w-[620px] h-[620px]
+        bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
+        rounded-[135px] rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
+
+      {/* Hide on mobile, show on larger screens | Bottom one */}
       <div
-        className="absolute -bottom-96 right-52  w-[620px] h-[620px]
-                      bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
-                      rounded-[135px] transform rotate-45 shadow-custom-shadow"
+        className="absolute -bottom-96 right-52  
+                  w-[620px] h-[620px]
+                  bg-gradient-to-r from-[#FF6340] via-[#6279ecd2] to-[#ffbd5a84]
+                  rounded-[135px] transform rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
-      <div className="h-full flex items-center justify-start ml-32 ">
+      <div className="h-full flex items-center justify-center lg:justify-start lg:ml-32">
         <LoginBox />
       </div>
     </section>
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -6,7 +6,8 @@ const page = () => {
   return (
     <section
       className="absolute top-0 left-0 w-full h-full overflow-hidden
-                bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-rose-200 to-white"
+                bg-[radial-gradient(ellipse_at_right,_var(--tw-gradient-stops))] from-rose-200 to-white
+                "
     >
       <Image
         className="absolute m-4 right-0"
@@ -16,31 +17,39 @@ const page = () => {
         width={250}
         height={200}
       />
+
+      {/* Hide on mobile, show on larger screens | Top large one */}
       <div
         className="absolute -top-60 left-28
-                    w-[230px] h-[650px] 
-                    bg-gradient-to-t from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
-                    rounded-full -rotate-45 shadow-custom-shadow"
+                  w-[230px] h-[650px] 
+                  bg-gradient-to-t from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
+                  rounded-full -rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
+
+      {/* Hide on mobile, show on larger screens | Top small one */}
       <div
         className="absolute top-40 left-0
-                    w-[100px] h-[350px] 
-                    bg-gradient-to-t from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
-                    rounded-full -rotate-45 shadow-custom-shadow"
+                  w-[100px] h-[350px] 
+                  bg-gradient-to-t from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
+                  rounded-full -rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
+
+      {/* Hide on mobile, show on larger screens | Coner one */}
       <div
         className="absolute -bottom-72 -left-14
-                    w-[230px] h-[650px] 
-                    bg-gradient-to-b from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
-                    rounded-full -rotate-45 shadow-custom-shadow"
+                  w-[230px] h-[650px] 
+                  bg-gradient-to-b from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
+                  rounded-full -rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
+
+      {/* Hide on mobile, show on larger screens | Bottom large one */}
       <div
         className="absolute -bottom-72 left-[500px]
-                    w-[230px] h-[650px] 
-                    bg-gradient-to-b from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
-                    rounded-full -rotate-45 shadow-custom-shadow"
+                  w-[230px] h-[650px] 
+                  bg-gradient-to-b from-[#FF6340] via-[#6279ecf3] to-[#ffbd5a84]
+                  rounded-full -rotate-45 shadow-custom-shadow hidden lg:block"
       ></div>
-      <div className="h-full flex items-center justify-end mr-32">
+      <div className="h-full flex items-center justify-center lg:justify-end lg:mr-32 ">
         <SignupBox />
       </div>
     </section>
