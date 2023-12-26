25b46a787ae526bc3f3679e736320bd976953851
Amethesh <ametheshlgp@gmail.com>
2023-12-26 14:13:44 +0530

    style: finish login,signup box style

--- src/components/pages/auth/LoginBox.tsx
+++ src/components/pages/auth/LoginBox.tsx
@@ -12,6 +12,18 @@ import {
 } from "@/components/ui/Form";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from "@/components/ui/Card";
+import { Linkedin } from "lucide-react";
+import googleLogo from "../../../../public/images/google-logo.svg";
+import linkedinLogo from "../../../../public/images/circle-linkedin.svg";
+import Image from "next/image";
 
 const formSchema = z.object({
   emailAddress: z.string().email(),
@@ -22,52 +34,84 @@ export const LoginBox = () => {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      emailAddress: ""
+      emailAddress: "",
+      password: ""
     }
   });
 
   const handleSubmit = () => {};
 
   return (
-    <section className="w-96 h-96 bg-background m-2 p-4 rounded-xl">
+    <Card className="w-[500px]">
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)}>
-          <FormField
-            control={form.control}
-            name="emailAddress"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Email </FormLabel>
-                  <FormControl>
-                    <Input placeholder="Email address" type="email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-          <FormField
-            control={form.control}
-            name="password"
-            render={({ field }) => {
-              return (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="Password" type="password" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              );
-            }}
-          />
-
-          <Button type="submit" variant={"primary"}>
-            Submit
-          </Button>
+          <CardHeader>
+            <CardTitle>Individual Login</CardTitle>
+            <CardDescription>Enter your email & password to login</CardDescription>
+          </CardHeader>
+          <div className="flex justify-between items-center px-4 w-full">
+            <Button className="w-full mx-2" variant={"outline"} size={"lg"}>
+              <Image className="mx-3" src={googleLogo} width={14} alt="Google" />
+              <p className="text-md">Google</p>
+            </Button>
+            <Button className="w-full mx-2" variant={"outline"} size={"lg"}>
+              <Image className="mx-3" src={linkedinLogo} width={20} alt="Google" />
+              <p>LinkedIn</p>
+            </Button>
+          </div>
+          <div className="flex items-center px-6 py-1">
+            <div className="border-t border-muted w-full my-4"></div>
+            <p className="mx-4 whitespace-nowrap text-muted text-xs">OR CONTINUE WITH</p>
+            <div className="border-t border-muted w-full my-4"></div>
+          </div>
+          <CardContent>
+            <FormField
+              control={form.control}
+              name="emailAddress"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email address" type="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => {
+                return (
+                  <FormItem>
+                    <FormLabel>Password </FormLabel>
+                    <FormControl>
+                      <Input placeholder="Password" type="password" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                );
+              }}
+            />
+          </CardContent>
+          <CardFooter className="flex justify-between ">
+            <Button className="w-full mx-2" variant={"secondary"}>
+              Cancel
+            </Button>
+            <button
+              type="submit"
+              className="bg-[#F7654B] w-full py-2 font-medium text-white rounded-md mx-2"
+            >
+              Login
+            </button>
+            {/* <Button type="submit" variant={"primary"}>
+              Sumbit
+            </Button> */}
+          </CardFooter>
         </form>
       </Form>
-    </section>
+    </Card>
   );
 };
