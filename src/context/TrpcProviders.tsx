569da4e2cabefe1a007685678c92b55244494851
vishnudarrshan <vishnuorp22@gmail.com>
2024-01-07 18:53:52 +0530

    fix: Added production url in trpc

--- src/context/TrpcProviders.tsx
+++ src/context/TrpcProviders.tsx
@@ -7,11 +7,13 @@ import { httpBatchLink } from "@trpc/client";
 
 const Providers = ({ children }: PropsWithChildren) => {
   const [queryClient] = useState(() => new QueryClient());
+  const baseUrl = process.env.PRODUCTION_URL;
+
   const [trpcClient] = useState(() =>
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc"
+          url: `${baseUrl}/api/trpc`
         })
       ]
     })
