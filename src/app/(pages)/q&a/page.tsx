92542afb3d2abb1ed3356bfbc481b300f9ad40e7
vishnudarrshan <vishnuorp22@gmail.com>
2024-01-08 14:38:27 +0530

    style: Styled the q&a with category based tabs

--- src/app/(pages)/q&a/page.tsx
+++ src/app/(pages)/q&a/page.tsx
@@ -1,31 +1,75 @@
-import SingleQA from '@/components/pages/q&a/SingleQA';
-import { getServerSession } from 'next-auth';
-import React from 'react';
+"use client";
+import SingleQA from "@/components/pages/q&a/SingleQA";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
+import React from "react";
 
-const Page = async () => {
+const Page = () => {
+  const [selectedTab, setSelectedTab] = React.useState("originality");
+  const handleChange = (value: string) => {
+    setSelectedTab(value);
+  };
   try {
-    const session = await getServerSession();
-    const data=["Have you independently contributed to original research that has gained national and/or international recognition over the past five years?",
-  "Did you publish publicly available outputs, such as books, papers, and conference papers, as the principal author during the specified 5-year period?",
-"Did you publish publicly available outputs, such as books, papers, and conference papers, as the principal author during the specified 5-year period?",
-"Did you publish publicly available outputs, such as books, papers, and conference papers, as the principal author during the specified 5-year period?",
-"Have you been externally awarded research grants and funding as a principal investigator in the last 5 years?",
-"Have you held staff responsibilities, including managing research staff, both currently and in previous roles?"]
+    const data = [
+      {
+        category: "originality",
+        questions: [
+          "Have you independently contributed to original research that has gained national and/or international recognition over the past five years?",
 
-    
-return (
-  <div className='absolute m-56 ml-72'>
-    {data.map((question, index) => (
-      
-      <SingleQA key={index} className="bg-[#F6F6F7]" question={question} questionNumber={index=0 + 1} />
-    ))}
-  </div>
-);
-} catch (error) {
-console.error('Error fetching session:', error);
-// Handle errors if necessary
-return <div>Error loading data</div>;
-}
+          "Did you publish publicly available outputs, such as books, papers, and conference papers, as the principal author during the specified 5-year period?",
+          "Have you been externally awarded research grants and funding as a principal investigator in the last 5 years?",
+          "Have you held staff responsibilities, including managing research staff, both currently and in previous roles?",
+          "Have you managed and graduated doctoral students in your current role?"
+        ]
+      },
+      {
+        category: "leadership",
+        questions: [
+          "Have you independently contributed to original research that has gained national and/or international recognition over the past five years?",
+
+          "Did you publish publicly available outputs, such as books, papers, and conference papers, as the principal author during the specified 5-year period?",
+          "Have you been externally awarded research grants and funding as a principal investigator in the last 5 years?",
+          "Have you held staff responsibilities, including managing research staff, both currently and in previous roles?",
+          "Have you managed and graduated doctoral students in your current role?",
+          "Are you gay"
+        ]
+      }
+    ];
+
+    const filteredQuestions = data.find((set) => set.category === selectedTab)?.questions || [];
+
+    return (
+      <div className="absolute m-56 ml-72">
+        <Tabs defaultValue="originality" className="w-[400px]" onValueChange={handleChange}>
+          <TabsList>
+            <TabsTrigger value="originality">Originality</TabsTrigger>
+            <TabsTrigger value="leadership">Leadership</TabsTrigger>
+          </TabsList>
+          <TabsContent value="originality">
+            {filteredQuestions.map((question, questionIndex) => (
+              <SingleQA
+                key={questionIndex}
+                question={question}
+                questionNumber={questionIndex + 1}
+              />
+            ))}
+          </TabsContent>
+          <TabsContent value="leadership">
+            {filteredQuestions.map((question, questionIndex) => (
+              <SingleQA
+                key={questionIndex}
+                question={question}
+                questionNumber={questionIndex + 1}
+              />
+            ))}
+          </TabsContent>
+        </Tabs>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching session:", error);
+    // Handle errors if necessary
+    return <div>Error loading data</div>;
+  }
 };
 
 export default Page;
--- src/components/pages/q&a/SingleQA.tsx
+++ src/components/pages/q&a/SingleQA.tsx
@@ -5,37 +5,41 @@ import {
   SelectContent,
   SelectItem,
   SelectTrigger,
-  SelectValue,
+  SelectValue
 } from "@/components/ui/Select";
 import { useState } from "react";
 
 interface SingleQAProps {
-  className: string;
   question: string;
-  questionNumber:number;
+  questionNumber: number;
 }
 
-export const SingleQA: React.FC<SingleQAProps> = ({ className, question,questionNumber }) => {
+export const SingleQA: React.FC<SingleQAProps> = ({ question, questionNumber }) => {
   const [selectedValue, setSelectedValue] = useState("");
-
-
+  const handleChange = (value: string) => {
+    setSelectedValue(value);
+  };
 
   return (
-    <div className={`flex border-2 w-[1279px] h-16 items-center justify-center rounded-lg mt-2 ${className}`}>
-        <div className="pl-4 w-[30px] mr-4 text-center text-white">{questionNumber}</div>
-      <div className="pl-4 w-[1030px] mr-12">{question}</div>
+    <div
+      className={`mt-2 flex h-16 w-[1279px] items-center justify-center rounded-lg border-2 ${
+        questionNumber % 2 == 0 ? "bg-white" : "bg-border"
+      }`}
+    >
+      <div className="mr-4 w-[30px] pl-4 text-center text-black">{questionNumber}</div>
+      <div className="mr-12 w-[1030px] pl-4">{question}</div>
       <div className="mr-4">
-        <Select>
-          <SelectTrigger 
+        <Select onValueChange={handleChange}>
+          <SelectTrigger
             className={`w-[150px] ${selectedValue === "yes" ? "bg-black text-white" : ""} ${
-              selectedValue === "no" ? "bg-muted_gray text-white" : "bg-black text-white"
+              selectedValue === "no" ? "bg-muted_gray text-white" : ""
             }`}
           >
             <SelectValue placeholder="Select" />
           </SelectTrigger>
-          <SelectContent >
-            <SelectItem value="yes" onClick={()=>setSelectedValue("yes")}>Yes</SelectItem>
-            <SelectItem value="no" onClick={()=>setSelectedValue("no")}>No</SelectItem>
+          <SelectContent>
+            <SelectItem value="yes">Yes</SelectItem>
+            <SelectItem value="no">No</SelectItem>
           </SelectContent>
         </Select>
       </div>
