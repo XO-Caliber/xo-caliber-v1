5b8c2ae5f2f2282af663afe51beaa51acc5cd12f
Amethesh <ametheshlgp@gmail.com>
2024-01-09 07:42:47 +0530

    feat: Adding assistant list

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,53 @@
+import { getAuthSession } from "@/app/api/auth/[...nextauth]/authOptions";
+import Home from "@/app/page";
+import Header from "@/components/Header";
+import { AddFirmForm } from "@/components/pages/admin/AddFirmForm";
+import { AddAssistantForm } from "@/components/pages/firm/AddAssistantForm";
+import { AssistantList } from "@/components/pages/firm/AssistantList";
+import { CardTitle } from "@/components/ui/Card";
 import React from "react";
-import Dashboard from "@/components/pages/dashboard/Dashboard";
 
-const Page = async () => {
+const Dashboard = async () => {
+  const session = await getAuthSession();
   return (
     <div>
-      <Dashboard />
+      {session ? (
+        <div className="ml-56 h-screen">
+          <div>
+            <Header className="ml-0">Dashboard</Header>
+          </div>
+          <div className="m-4">
+            <div className="w-full font-bold">
+              <CardTitle>Welcome {session?.user?.name?.toLowerCase()}</CardTitle>
+            </div>
+            <div className="m-4 w-[350px]">
+              {session.user.role === "ADMIN" ? (
+                <div>
+                  <AddFirmForm />
+                </div>
+              ) : (
+                <></>
+              )}
+              {session.user.role === "FIRM" ? (
+                <div className="flex w-full items-center justify-center">
+                  <AddAssistantForm />
+
+                  <AssistantList />
+                </div>
+              ) : (
+                <></>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <Header>Dashboard</Header>
+          <Home />
+        </div>
+      )}
     </div>
   );
 };
 
-export default Page;
+export default Dashboard;
---
+++ src/components/pages/firm/AssistantList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const AssistantList = () => {
+  return <div>AssistantList</div>;
+};
