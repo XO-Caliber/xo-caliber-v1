b99b48f66a1e782b01b622c8d816bc8da6f451a3
vishnudarrshan <vishnuorp22@gmail.com>
2024-01-15 17:02:47 +0530

    fix: Changed single QA box size as flex

--- src/components/pages/q&a/GetSingleQA.tsx
+++ src/components/pages/q&a/GetSingleQA.tsx
@@ -21,17 +21,17 @@ export const GetSingleQA: React.FC<SingleQAProps> = ({ question, questionNumber 
 
   return (
     <div
-      className={`mt-2 flex h-16 w-[1500px] items-center justify-center rounded-lg border-2 ${
+      className={`mt-2 flex  w-[1500px] items-center justify-center rounded-lg  border-2 ${
         questionNumber % 2 == 0 ? "bg-white" : "bg-border"
       }`}
     >
       <div className="mr-4 w-[30px] pl-4 text-center text-black">{questionNumber}</div>
-      <div className="mr-8 w-[1200px] pl-4">{question}</div>
+      <div className="mr-8 w-[1200px] p-4 pl-4">{question}</div>
       <div className="mr-4">
         <Select onValueChange={handleChange}>
           <SelectTrigger
             className={`w-[150px] ${selectedValue === "yes" ? "bg-black text-white" : ""} ${
-              selectedValue === "no" ? "bg-muted_gray text-white" : ""
+              selectedValue === "no" ? "bg-[#64748B] text-white" : ""
             }`}
           >
             <SelectValue placeholder="Select" />
