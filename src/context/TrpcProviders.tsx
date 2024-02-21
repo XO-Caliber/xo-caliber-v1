654ed38df1db11cadbec2fc6da3d553d66545588
Vishnudarrshan O R P <vishnuorp22@gmail.com>
2024-02-21 11:54:30 +0530

    Uncommiting last commit

--- src/context/TrpcProviders.tsx
+++ src/context/TrpcProviders.tsx
@@ -14,7 +14,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `api/trpc`
+          url: `starfish-app-c34pl.ondigitalocean.app/api/trpc`
         })
       ]
     })
