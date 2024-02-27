5b159fb92694a7e85f648f9e5d6bf018a304cfa9
vishnudarrshan <vishnuorp22@gmail.com>
2024-02-27 15:04:10 +0530

    fix: Fixed firm side QA

--- src/components/pages/q&a/firm/FirmQA.tsx
+++ src/components/pages/q&a/firm/FirmQA.tsx
@@ -1,26 +1,8 @@
-"use client";
-import UserSelectList from "@/components/utils/UserSelectList";
-import { AssistantDialog } from "../addAssistant/AssistantDialog";
-import AddQADiaglog from "../addQA/AddQADiaglog";
-import AddCategoryDialog from "../addCategory/AddCategoryDialog";
 import { ViewFirmQA } from "../viewQA/firm/ViewFirmQA";
-import ImportAdmin from "./ImportAdmin";
-import { use } from "react";
 
 const FirmQA = () => {
   return (
-    <div className="m-4 ml-56 text-xl ">
-      <div className="flex items-center justify-start ">
-        <div className="px-4">
-          <h1 className="text-2xl font-bold">Welcome back!</h1>
-          <p className="text-sm font-normal text-muted">Here’s a list of Bonny davis’s cases</p>
-        </div>
-        <div className="ml-32">
-          <AssistantDialog />
-        </div>
-
-        <ImportAdmin />
-      </div>
+    <div className=" ml-56 text-xl ">
       <ViewFirmQA />
     </div>
   );
