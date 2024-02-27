b982923d2a18454b7fe4b8e472c0fadff9fbeac3
vishnudarrshan <vishnuorp22@gmail.com>
2024-02-27 18:34:39 +0530

    style: Changed assistant graph style

--- src/components/pages/spider-graph/assistant/AssistantGraph.tsx
+++ src/components/pages/spider-graph/assistant/AssistantGraph.tsx
@@ -150,8 +150,11 @@ function AssistantGraph({ userType }: userType) {
 
   return (
     <>
-      {/* Radar chart */}
-      <ResizablePanelGroup direction="horizontal" className="min-h-screen max-w-full">
+      <div className="flex h-[68px] items-center justify-between border-2 border-l-0">
+        <p className="m-4 mt-[1.2rem] text-xl font-bold text-muted">Spider Graph</p>{" "}
+        <AssistantUserSelect getSelectedUser={getSelectedUser} />
+      </div>
+      <ResizablePanelGroup direction="horizontal" className="min-h-[93vh] max-w-full">
         <ResizablePanel defaultSize={50} className="m-2">
           <main className="flex h-full flex-col p-6">
             <div className="flex items-center justify-between">
@@ -161,7 +164,6 @@ function AssistantGraph({ userType }: userType) {
                   Here’s a list of Bonny davis’s cases
                 </p>
               </div>
-              <AssistantUserSelect getSelectedUser={getSelectedUser} />
             </div>
             <div className="m-2 h-full rounded-xl border border-red-600">
               <canvas id="myChart">myChart</canvas>
@@ -171,10 +173,10 @@ function AssistantGraph({ userType }: userType) {
         <ResizableHandle withHandle />
         <ResizablePanel defaultSize={50}>
           <ResizablePanelGroup direction="vertical">
-            <ResizableHandle withHandle />
             <ResizablePanel defaultSize={50} className="m-3">
               <ClientsNote selectedUser={user} />
             </ResizablePanel>
+            <ResizableHandle withHandle />
             <ResizablePanel defaultSize={50} className="m-3">
               <FirmsNote selectedUser={user} />
             </ResizablePanel>
