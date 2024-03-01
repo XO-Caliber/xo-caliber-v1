56e2b5cf7e4d400c9c77338590ccfd16db6e3596
vishnudarrshan <vishnuorp22@gmail.com>
2024-03-02 04:37:45 +0530

    fix: Removed pdf downloading option

--- src/components/pages/q&a/viewQA/assistant/DownloadAssistantQuestions.tsx
+++ src/components/pages/q&a/viewQA/assistant/DownloadAssistantQuestions.tsx
@@ -21,44 +21,44 @@ const DownloadAdminQuestions = () => {
   const [type, setType] = useState("");
   const handleChange = (type: string) => {
     setType(type);
-    type === "pdf" && exportHandler();
+    // type === "pdf" && exportHandler();
     type === "xlsx" && exportExcelHandler();
     setType("");
   };
-  const exportHandler = () => {
-    if (!categoriesList || categoriesList.length === 0) {
-      console.error("No data available to export.");
-      return;
-    }
+  // const exportHandler = () => {
+  //   if (!categoriesList || categoriesList.length === 0) {
+  //     console.error("No data available to export.");
+  //     return;
+  //   }
 
-    const doc = new jsPDF();
+  //   const doc = new jsPDF();
 
-    const tableData = categoriesList.map((category) => [
-      category.name,
-      category.questions.map((question) => question.question).join("\n"),
-      category.questions.map((question) => question.mark).join("\n")
-    ]);
-    //@ts-ignore
-    doc.autoTable({
-      head: [["CATEGORY", "QUESTIONS", "WEIGHTAGE"]],
-      body: tableData,
-      styles: {
-        font: "helvetica",
-        fontSize: 12,
-        cellPadding: 2,
-        textColor: [0, 0, 0], // Black color
-        fillColor: [255, 255, 255], // White color
-        lineColor: [0, 0, 0], // Black color
-        lineWidth: 0.1
-      },
-      headStyles: {
-        fillColor: [192, 192, 192], // Light gray color for header row
-        textColor: [0, 0, 0] // Black color for header row text
-      }
-    });
+  //   const tableData = categoriesList.map((category) => [
+  //     category.name,
+  //     category.questions.map((question) => question.question).join("\n"),
+  //     category.questions.map((question) => question.mark).join("\n")
+  //   ]);
+  //   //@ts-ignore
+  //   doc.autoTable({
+  //     head: [["CATEGORY", "QUESTIONS", "WEIGHTAGE"]],
+  //     body: tableData,
+  //     styles: {
+  //       font: "helvetica",
+  //       fontSize: 12,
+  //       cellPadding: 2,
+  //       textColor: [0, 0, 0], // Black color
+  //       fillColor: [255, 255, 255], // White color
+  //       lineColor: [0, 0, 0], // Black color
+  //       lineWidth: 0.1
+  //     },
+  //     headStyles: {
+  //       fillColor: [192, 192, 192], // Light gray color for header row
+  //       textColor: [0, 0, 0] // Black color for header row text
+  //     }
+  //   });
 
-    doc.save("AdminQuestions.pdf");
-  };
+  //   doc.save("AdminQuestions.pdf");
+  // };
   const exportExcelHandler = () => {
     if (!categoriesList || categoriesList.length === 0) {
       console.error("No data available to export.");
@@ -128,7 +128,7 @@ const DownloadAdminQuestions = () => {
           <SelectValue placeholder="Export as" />
         </SelectTrigger>
         <SelectContent>
-          <SelectItem value="pdf">Pdf</SelectItem>
+          {/* <SelectItem value="pdf">Pdf</SelectItem> */}
           <SelectItem value="xlsx">Excel sheet</SelectItem>
         </SelectContent>
       </Select>
