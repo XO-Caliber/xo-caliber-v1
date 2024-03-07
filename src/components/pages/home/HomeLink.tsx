4ce7a5ccec2dc28e26a9374b5e237534e5af039c
Amethesh <ametheshlgp@gmail.com>
2024-03-07 11:11:44 +0530

    style: User home page style

--- src/components/pages/home/HomeLink.tsx
+++ src/components/pages/home/HomeLink.tsx
@@ -1,30 +1,43 @@
 import { CandlestickChart, CheckSquare, FileEdit, Wind } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 export const HomeLink = (user: { name: string | null | undefined }) => {
   return (
     <div>
-      <li className="ml-8 w-full pl-4 pt-4 font-bold">
+      <li className="ml-4 w-full pl-4 pt-4 font-bold">
         <h1 className="text-2xl font-bold ">Welcome {user.name}</h1>
         <h2 className="text-sm font-normal ">Here is the overview</h2>
       </li>
-      <li className="ml-8 grid w-max grid-cols-2 justify-start text-nowrap p-2 text-sm">
-        <span className="m-2 flex h-20 w-32 flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary">
+      <li className="ml-4 grid w-max grid-cols-2 justify-start text-nowrap p-2 text-sm">
+        <Link
+          href={"/workspaces/xo-caliberq&a"}
+          className="m-2 flex h-20 w-32 cursor-pointer flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary"
+        >
           <CheckSquare size={20} className="mb-2" />
           <h1>XO Caliber Q&A</h1>
-        </span>
-        <span className="m-2 flex h-20 w-32 flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary">
+        </Link>
+        <Link
+          href={"/workspaces/xo-coverletter"}
+          className="m-2 flex h-20 w-32 cursor-pointer flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary"
+        >
           <FileEdit size={20} className="mb-2" />
           <h1>XO Cover Letter</h1>
-        </span>
-        <span className="m-2 flex h-20 w-32 flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary">
+        </Link>
+        <Link
+          href={"/workspaces/xo-mindmap"}
+          className="m-2 flex h-20 w-32 cursor-pointer flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary"
+        >
           <Wind size={20} className="mb-2" />
           <h1>XO Mind Map</h1>
-        </span>
-        <span className="m-2 flex h-20 w-32 flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary">
+        </Link>
+        <Link
+          href={"/workspaces/xo-spidergraph"}
+          className="m-2 flex h-20 w-32 cursor-pointer flex-col items-center justify-center rounded-md border-2 border-border text-center hover:border-primary"
+        >
           <CandlestickChart size={20} className="mb-2" />
           <h1>XO Spider Graph</h1>
-        </span>
+        </Link>
       </li>
     </div>
   );
--- src/components/pages/home/client/ClientFirmList.tsx
+++ src/components/pages/home/client/ClientFirmList.tsx
@@ -14,7 +14,7 @@ const ClientFirmList = () => {
   };
   if (clientFirmList.data?.Firm?.firmId) {
     return (
-      <section className="scrollableContainer flex h-72 w-[310px] flex-col items-center  overflow-y-scroll">
+      <section className="scrollableContainer flex h-min w-[310px] flex-col items-center  overflow-y-scroll">
         <div className=" m-4 ml-6 flex flex-col items-center justify-center">
           {/* <h2 className="pb-4 text-xl font-semibold">List of all Assistant:</h2> */}
           <div className="  grid-rows mb-28 grid  w-full gap-y-5">
@@ -31,7 +31,7 @@ const ClientFirmList = () => {
   } else {
     // If the user does not exist, return an error message
     return (
-      <div className=" scrollableContainer flex min-h-72 w-[310px] flex-col items-center overflow-y-scroll">
+      <div className=" scrollableContainer flex h-max max-h-72 w-[310px] flex-col items-center overflow-y-scroll">
         {[...Array(1)].map((_, index) => (
           <div key={index} className=" grid-rows grid w-full  gap-y-5 px-12">
             <UserProfileLoading />
