cbea0e3f08fd46533ea083f080f7013079f4a347
Amethesh <ametheshlgp@gmail.com>
2024-03-09 19:49:16 +0530

    change: dev to prod url

--- src/context/TrpcProviders.tsx
+++ src/context/TrpcProviders.tsx
@@ -14,7 +14,7 @@ const Providers = ({ children }: PropsWithChildren) => {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: `http://localhost:3000/api/trpc`
+          url: `https://xo-caliber-v1-xi.vercel.app/api/trpc`
         })
       ]
     })
